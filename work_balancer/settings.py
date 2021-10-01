0318840dd6814af194e50413f006a011eebff594
nikitazigman <zigman.nikita@gmail.com>
2021-10-01 22:17:26 +0200

    switched db to postgresql, added docker

--- work_balancer/settings.py
+++ work_balancer/settings.py
@@ -103,8 +103,12 @@ WSGI_APPLICATION = 'work_balancer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('POSTGRES_NAME'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': env('POSTGRES_PORT')
     }
 }
 
